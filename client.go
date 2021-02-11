3f8f6364833acd52f50717d645050f162ae246b8
Muhi98 <muhammed.akcan@gmx.de>
2021-02-11 22:09:26 +0100

    listener auskommentiert

--- client.go
+++ client.go
@@ -34,7 +34,6 @@ import (
 	"perun.network/go-perun/wallet"
 	"perun.network/go-perun/wire"
 	"perun.network/go-perun/wire/net"
-	"perun.network/go-perun/wire/net/simple"
 )
 
 type (
@@ -169,8 +168,8 @@ func NewClient(ctx *Context, cfg *Config, w *Wallet) (*Client, error) {
 	log.Println("go-wrapper, client.go, NewClient, 1.5")
 
 	endpoint := fmt.Sprintf("%s:%d", cfg.IP, cfg.Port)
-	listener, err := simple.NewTCPListener(endpoint)
-	//listener, err := NewTCPListenerP2P(host)
+	//listener, err := simple.NewTCPListener(endpoint)
+	listener, err := NewTCPListenerP2P(host)
 	if err != nil {
 		return nil, errors.WithMessagef(err, "listening on %s", endpoint)
 	}
