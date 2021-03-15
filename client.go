b5dd723fab23398c4b5582c52dca69f9c653fdd5
Muhsin Abboud <muhsin.abboud@web.de>
2021-03-15 11:38:05 +0100

    Update Server ID

--- client.go
+++ client.go
@@ -38,7 +38,7 @@ import (
 )
 
 // peer id of the relay server
-const serverID = "QmVCPfUMr98PaaM8qbAQBgJ9jqc7XHpGp7AsyragdFDmgm"
+const serverID = "QmPyRxsUQfAWR6uYYkSoZQsaM1pra2qpUHE3CMTgrfsTEV" //  QmVCPfUMr98PaaM8qbAQBgJ9jqc7XHpGp7AsyragdFDmgm
 
 type (
 	// Client is a state channel client. It is the central controller to interact
